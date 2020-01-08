730d682515d583414743b474af047e523b1ceb3d
FritzHeider <fritz.heider@students.makeschool.com>
2020-01-08 10:04:01 -0800

    first

---
+++ WShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WShop
+//
+//  Created by Fritz Heider on 1/8/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
